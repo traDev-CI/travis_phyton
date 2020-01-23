2ff13bbd049df16fd1fcba5ab9cca6a5a2ede4c2
Alfredo Rivas Jimenez <alfredo.rivas@stkprojects.com>
2020-01-23 16:27:56 -0600

    My first commit

--- test_fruit.py
+++ test_fruit.py
@@ -1,1 +1,6 @@
-print("Hola Mundo")
+print ("Tu primer número es: 30")
+print ("Tu segundo número es: 50")
+print ("Su multiplicación es")
+primerNumero= 30
+segundoNumero= 50
+print (primerNumero * segundoNumero)
